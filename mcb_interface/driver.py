4c04f1bd94fdd32b004d6f283066f66d212f36c2
Bradley Powers <brad@lowellmakes.com>
2017-09-08 01:12:01 +0000

    Add gitignore.  Add simple exit handler to driver that zeroes command.

--- mcb_interface/driver.py
+++ mcb_interface/driver.py
@@ -3,13 +3,24 @@
 from romi import Romi
 romi = Romi()
 from time import sleep
-# from math import pi
+import atexit
+import time
+from math import pi
 
+start_time = time.time()
+
+romi.reset_odometry()
+
+def at_exit(romi):
+    romi.velocity_command(0, 0)
+    print time.time() - start_time
+
+atexit.register(at_exit, romi = romi)
 
 while True:
     battery_millivolts = romi.read_battery_millivolts()
     v_x, v_theta, x, y, theta = romi.read_odometry()
-    romi.velocity_command(1.0, 0)
+    romi.velocity_command(0.0, pi*100)
 
     print "Battery Voltage: ", battery_millivolts[0], " Volts."
     print "Vx: ", v_x, " m/s"
